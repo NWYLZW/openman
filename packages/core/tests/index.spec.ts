e3a4b44e38cac690c8c8da4dd1f4f4335a89e9d8
yijie4188 <yijie4188@gmail.com>
2022-12-08 10:06:37 +0800

    fix: type error

--- packages/core/tests/index.spec.ts
+++ packages/core/tests/index.spec.ts
@@ -1,15 +1,15 @@
 import { generate } from '@openman/core'
-import { 性格 } from '../src'
 
 describe('generate', function () {
   it('should create a man', () => {
     const content = generate({
-      名字: '白菲酱',
-      年龄: '不可以说的秘密',
-      身高: 178,
+      名字: '未名酱',
+      年龄: 19,
+      身高: 168,
       体重: 48,
       三围: [90, 60, 90],
-      性格: 性格.可爱,
+      性格: '可爱',
+      种族: '猫娘'
     })
     console.log(content)
   })
