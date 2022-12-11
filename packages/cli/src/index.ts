32bce671d415b68e1e3509a3fae78b89a0920bce
yijie4188 <yijie4188@gmail.com>
2022-12-11 16:00:56 +0800

    chore(cli): use commonjs

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,9 +1,9 @@
-import { ChatGPTAPI } from 'chatgpt'
+import { ChatGPTAPI } from 'chatgpt-commonjs'
 
 export async function example() {
   const api = new ChatGPTAPI({
     sessionToken: process.env.CHATGPT_SESSION_TOKEN,
   })
-  const response = await api.sendMessage('Hello')
+  const response = await api.sendMessage('你好')
   console.log(response)
 }
